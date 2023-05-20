b6a5c3576f9006bd1142eafee676183abb21f0af
MikleSolovyev <115595582+MikleSolovyev@users.noreply.github.com>
2023-05-20 19:47:45 +0300

    add reading from files

--- main.py
+++ main.py
@@ -1,25 +1,42 @@
 import os
-import webbrowser
 
 from modules import markov_algo as algo, html_editor as he, graph_builder as gb
 
-GRAPH_PATH = os.path.join(os.getcwd(), 'graph.html')
+CODES_PATH = os.path.join(os.getcwd(), 'codes.txt')
+GRAPHS_DIR_PATH = os.path.join(os.getcwd(), 'graphs')
+GRAPH_PREFIX_NAME = 'graph'
 
 
 def parse_input():
-    codes = input('Введите коды через пробел: ').split()
-    while len(codes) < 1 or len(set(codes)) < len(codes):
-        print('Неверные входные данные!')
-        codes = input('Введите коды через пробел: ').split()
+    codes_path = input(r'Введите путь до файла с кодами (пустой ввод - .\codes.txt): ')
+    if not codes_path:
+        codes_path = CODES_PATH
+    while not os.path.exists(codes_path):
+        print('Нету файла по такому пути!')
+        codes_path = input(r'Введите путь до файла с кодами (пустой ввод - .\codes.txt): ')
+        if not codes_path:
+            codes_path = CODES_PATH
 
-    return codes
+    return codes_path
 
 
 if __name__ == '__main__':
-    codes = parse_input()
+    codes_path = parse_input()
 
-    graph, loop_decodings = algo.run(codes)
-    gb.build_G(graph, GRAPH_PATH)
-    he.make_changes(GRAPH_PATH, codes, graph, loop_decodings)
+    if not os.path.exists(GRAPHS_DIR_PATH):
+        os.mkdir(GRAPHS_DIR_PATH)
 
-    webbrowser.open(GRAPH_PATH)
+    with open(codes_path, 'r') as file:
+        line_index = 0
+        for line in file:
+            line_index += 1
+
+            codes = line.split()
+            if len(codes) < 1 or len(set(codes)) < len(codes):
+                print(f'Строка с кодами под номером {line_index} содержит неправильную последовательность кодов!')
+                continue
+
+            graph_path = os.path.join(GRAPHS_DIR_PATH, f'{GRAPH_PREFIX_NAME}_{line_index}.html')
+            graph, loop_decodings = algo.run(codes)
+            gb.build_G(graph, graph_path)
+            he.make_changes(graph_path, codes, graph, loop_decodings)
